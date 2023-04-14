708e013c8a26af079402a3657a31e3393a16ab49
ali1924 <mohammadali229987@gmail.com>
2023-04-14 08:02:23 +0600

    Register compnents working progress

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './Register.css';
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleEmail = (event) => {
+        const email = event.target.value;
+        // console.log(email);
+        setEmail(email);
+    }
+    // console.log(email);
+
+    const handlePassword = (event) => {
+        const password = event.target.value;
+        // console.log(password);
+        setPassword(password);
+    }
+    // console.log(password);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(email,password);
+    }
     return (
-        <div>
-            <p>This is register</p>
+        <div className='register-form'>
+            <h1>Pleaser Register.....!!</h1>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleEmail} type="email" name="email" id="email" placeholder='Your Email' />
+                <br />
+                <input onBlur={handlePassword} type="password" name="password" id="password" placeholder='Your Password'/>
+                <br />
+                <input type="submit" value="register" />
+            </form>
         </div>
     );
 };
