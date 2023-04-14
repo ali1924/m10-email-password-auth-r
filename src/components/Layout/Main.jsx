2224ccb751b40e24a0a39168cb1ff4d53e0e471c
ali1924 <mohammadali229987@gmail.com>
2023-04-14 07:35:55 +0600

    nested route done

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from '../Header/Header';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
             <p>This is Main page</p>
+            <Header></Header>
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Main from './components/Layout/Main'
+import Home from './components/Home/Home'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
+import RegisterRBS from './components/RegisterRBS/RegisterRBS'
+import RegisterBS from './components/RegisterBS/RegisterBS'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element:<Home></Home>
+      },
+      {
+        path: '/login',
+        element:<Login></Login>
+      },
+      {
+        path: '/register',
+        element:<Register></Register>
+      },
+      {
+        path: '/register-rbs',
+        element:<RegisterRBS></RegisterRBS>
+      },
+      {
+        path: '/register',
+        element:<RegisterBS></RegisterBS>
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <RouterProvider router={router}></RouterProvider>
 )
